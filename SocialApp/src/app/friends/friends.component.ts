56e1c66812858cfaf0e5568774e14c108b35c494
FighterSoul <hedi.abdessalem@gmail.com>
2023-11-16 05:42:30 +0100

    building responsive css

--- SocialApp/src/app/friends/friends.component.ts
+++ SocialApp/src/app/friends/friends.component.ts
@@ -10,14 +10,22 @@ import { CommonModule } from '@angular/common';
 })
 export class FriendsComponent {
   friends = [
-    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg' },
-    { name: 'Jane', bio: 'Consectetur adipiscing elit', img: 'https://img.freepik.com/free-photo/young-beautiful-woman-with-long-hair_1150-16098.jpg' },
-    { name: 'Bob', bio: 'Sed do eiusmod tempor incididunt', img: 'https://img.freepik.com/free-photo/portrait-handsome-man-with-beard_23-2148221552.jpg' },
-    { name: 'Bobse', bio: 'Sed do eiusmod tempor incididunt', img: 'https://img.freepik.com/free-photo/portrait-handsome-man-with-beard_23-2148221552.jpg' },
-    { name: 'Bobsr', bio: 'Sed do eiusmod tempor incididunt', img: 'https://img.freepik.com/free-photo/portrait-handsome-man-with-beard_23-2148221552.jpg' },
-    { name: 'Bobst', bio: 'Sed do eiusmod tempor incididunt', img: 'https://img.freepik.com/free-photo/portrait-handsome-man-with-beard_23-2148221552.jpg' },
-    { name: 'Bobst', bio: 'Sed do eiusmod tempor incididunt', img: 'https://img.freepik.com/free-photo/portrait-handsome-man-with-beard_23-2148221552.jpg' },
-    { name: 'Bobst', bio: 'Sed do eiusmod tempor incididunt', img: 'https://img.freepik.com/free-photo/portrait-handsome-man-with-beard_23-2148221552.jpg' }
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
+    { name: 'John', bio: 'Lorem ipsum dolor sit amet' ,img: '../../assets/images/drac.png' },
   ];
 }
 
